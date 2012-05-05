c737f3d71d06a8afb79bff9fb812b7b070c747ad
Simon Hitzemann <simon-github@hitzemann.org>
2012-05-05 14:46:47 +0200

    Checking for PING/AXFR so they produce correct answers.

--- rev.pl
+++ rev.pl
@@ -30,31 +30,31 @@ my %v2b = do {
 my %b2v = reverse %v2b;
 
 sub from32 {
-  my $str = shift;
-  $str =~ tr/ybndrfg8ejkmcpqxot1uwisza345h769//cd;
-  $str =~ s/(.)/$v2b{$1}/g;
-  my $padlen = (length $str) % 8;
-  $str =~ s/0{$padlen}\z//;
-  return scalar pack "B*", $str;
+    my $str = shift;
+    $str =~ tr/ybndrfg8ejkmcpqxot1uwisza345h769//cd;
+    $str =~ s/(.)/$v2b{$1}/g;
+    my $padlen = (length $str) % 8;
+    $str =~ s/0{$padlen}\z//;
+    return scalar pack "B*", $str;
 }
 
 sub to32 {
-  my $str = shift;
-  my $ret = unpack "B*", $str;
-  $ret .= 0 while ( length $ret ) % 5;
-  $ret =~ s/(.....)/$b2v{$1}/g;
-  return $ret;
+    my $str = shift;
+    my $ret = unpack "B*", $str;
+    $ret .= 0 while ( length $ret ) % 5;
+    $ret =~ s/(.....)/$b2v{$1}/g;
+    return $ret;
 }
 
 sub from16 {
-  my $str = shift;
-  $str =~ tr/0-9a-f//cd;
-  return scalar pack "H*", lc $str;
+    my $str = shift;
+    $str =~ tr/0-9a-f//cd;
+    return scalar pack "H*", lc $str;
 }
 
 sub to16 {
-  my $str = shift;
-  return unpack "H*", $str;
+    my $str = shift;
+    return unpack "H*", $str;
 }
 
 $|=1;
@@ -68,125 +68,132 @@ my $helo = <>;
 chomp($helo);
 
 unless ($helo =~ /HELO\t[12]/) {
-        print "FAIL\n";
-        while(<>) {};
-        exit;
+    print "FAIL\n";
+    while(<>) {};
+    exit;
 }
 
 my $domains;
 
 # Build domain table based on configuration
 while(my ($dom,$prefix) = each %$domaintable) {
-        $domains->{$dom} = $prefix;
+    $domains->{$dom} = $prefix;
 
-	# build reverse lookup domain
-        my $tmp = $prefix;
-        $tmp=~s/://g;
-	$prefix = $tmp;
+    # build reverse lookup domain
+    my $tmp = $prefix;
+    $tmp=~s/://g;
+    $prefix = $tmp;
 
-	# this is needed for compression
-        my $bits = length($tmp)*4;
+    # this is needed for compression
+    my $bits = length($tmp)*4;
 
-        $tmp = join '.', reverse split //,$tmp;
-        $tmp=~s/^[.]//;
-        $tmp=~s/[.]$//;
-	
-	# forward lookup
-        $domains->{$dom} = { prefix => $prefix, bits => $bits };
-	# reverse lookup
-        $domains->{"$tmp.ip6.arpa"} = { domain => $dom, bits => $bits };
+    $tmp = join '.', reverse split //,$tmp;
+    $tmp=~s/^[.]//;
+    $tmp=~s/[.]$//;
 
-	# ensure the n. of bits is divisable by 16 (otherwise bad stuff happens)
-        unless (($bits%16)==0) {
-		print "OK\t$dom has $prefix that is not divisable with 8\n";
-		while(<>) {
-			print "END\n";
-		};
-		exit 0;
-	}
+    # forward lookup
+    $domains->{$dom} = { prefix => $prefix, bits => $bits };
+    # reverse lookup
+    $domains->{"$tmp.ip6.arpa"} = { domain => $dom, bits => $bits };
+
+    # ensure the n. of bits is divisable by 16 (otherwise bad stuff happens)
+    unless (($bits%16)==0) {
+        print "OK\t$dom has $prefix that is not divisable with 8\n";
+        while(<>) {
+            print "END\n";
+        };
+        exit 0;
+    }
 }
 
 print "OK\tAutomatic reverse generator v1.0 starting\n";
 
 while(<>) {
-	chomp;
-	my @arr=split(/\t/);
-	if(@arr<6) {
-		print "LOG\tPowerDNS sent unparseable line\n";
-		print "FAIL\n";
-		next;
-	}
+    chomp;
+    my @arr=split(/\t/);
+    if($arr[0] eq 'Q' && @arr<6) {
+        print "LOG\tPowerDNS sent unparseable line\n";
+        print "FAIL\n";
+        next;
+    }
 
-	# get the request
-	my ($type,$qname,$qclass,$qtype,$id,$ip)=@arr;
+    if ($arr[0] eq 'PING' or $arr[0] eq 'AXFR' {
+            print "LOG\tPING or AXFR requested\n";
+        } elsif ($arr[0] eq 'Q') {
 
-	print "LOG\t$qname $qclass $qtype?\n" if ($debug);
+            # get the request
+            my ($type,$qname,$qclass,$qtype,$id,$ip)=@arr;
 
-	# forward lookup handler
-	if (($qtype eq 'AAAA' || $qtype eq 'ANY') && $qname=~/${nodeprefix}([^.]*).(.*)/) {
-		my $node = $1;
-		my $dom = $2;
+            print "LOG\t$qname $qclass $qtype?\n" if ($debug);
 
-		print "LOG\t$node $dom and ", $domains->{$dom}{prefix}, "\n" if ($debug);
+            # forward lookup handler
+            if (($qtype eq 'AAAA' || $qtype eq 'ANY') && $qname=~/${nodeprefix}([^.]*).(.*)/) {
+                my $node = $1;
+                my $dom = $2;
 
-		# make sure it's our domain first and reasonable
-		if ($domains->{$dom} and $node=~m/^[ybndrfg8ejkmcpqxot1uwisza345h769]+$/) {
-			my $n = (128 - $domains->{$dom}{bits}) / 5;
+                print "LOG\t$node $dom and ", $domains->{$dom}{prefix}, "\n" if ($debug);
 
-			while(length($node) < $n) {
-				$node = join '', "y", $node;
-			}
+                # make sure it's our domain first and reasonable
+                if ($domains->{$dom} and $node=~m/^[ybndrfg8ejkmcpqxot1uwisza345h769]+$/) {
+                    my $n = (128 - $domains->{$dom}{bits}) / 5;
 
-			$node = to16(from32($node));
+                    while(length($node) < $n) {
+                        $node = join '', "y", $node;
+                    }
 
-			$n = (128 - $domains->{$dom}{bits}) / 4;
+                    $node = to16(from32($node));
 
-			# only process correct length
-			if (length($node) == $n) {
-				# convert
-				my $dname = $node;
-				# hmm
-				my $tmp = $domains->{$dom}{prefix};
-					
-				# build whole IPv6 address and add : to correct placs
-				$dname = $tmp.$dname;
-				$dname=~s/(.{4})/$1:/g;
-				chop $dname;
-	
-				# reply with value
-                                print "LOG\t$qname\t$qclass\tAAAA\t$ttl\t$id\t$dname\n" if ($debug);
-				print "DATA\t$qname\t$qclass\tAAAA\t$ttl\t$id\t$dname\n";
-			}
-		}
-	# reverse lookup
-	} elsif (($qtype eq 'PTR' || $qtype eq 'ANY') && $qname=~/(.*\.arpa$)/) {
-		my $node = $1;
+                    $n = (128 - $domains->{$dom}{bits}) / 4;
 
-		# look for our domain
-		foreach(keys %$domains) {
-			my $key = $_;
-			my $dom = $domains->{$_}{domain};
-                        my $index = index($node, $key);
-                        if ($index != -1) {
-				$qname = $node;
-                                $node = substr($qname, 0, $index-1);
+                    # only process correct length
+                    if (length($node) == $n) {
+                        # convert
+                        my $dname = $node;
+                        # hmm
+                        my $tmp = $domains->{$dom}{prefix};
 
-				$node = join '', reverse split /\./, $node;
+                        # build whole IPv6 address and add : to correct placs
+                        $dname = $tmp.$dname;
+                        $dname=~s/(.{4})/$1:/g;
+                        chop $dname;
 
-				# recode to base32
-				$node = to32(from16($node));
+                        # reply with value
+                        print "LOG\t$qname\t$qclass\tAAAA\t$ttl\t$id\t$dname\n" if ($debug);
+                        print "DATA\t$qname\t$qclass\tAAAA\t$ttl\t$id\t$dname\n";
+                    }
+                }
+                # reverse lookup
+            } elsif (($qtype eq 'PTR' || $qtype eq 'ANY') && $qname=~/(.*\.arpa$)/) {
+                my $node = $1;
 
-				# compress
-				$node =~ s/^y*//;
-				$node = 'y' if ($node eq '');
+                # look for our domain
+                foreach(keys %$domains) {
+                    my $key = $_;
+                    my $dom = $domains->{$_}{domain};
+                    my $index = index($node, $key);
+                    if ($index != -1) {
+                        $qname = $node;
+                        $node = substr($qname, 0, $index-1);
 
-				print "LOG\t$qname\t$qclass\tPTR\t$ttl\t$id\t$nodeprefix$node.$dom\n" if ($debug);
-				print "DATA\t$qname\t$qclass\tPTR\t$ttl\t$id\t$nodeprefix$node.$dom\n";
-			}
-		}
-	}
-	
-	#end of data
-	print "END\n";
-}
+                        $node = join '', reverse split /\./, $node;
+
+                        # recode to base32
+                        $node = to32(from16($node));
+
+                        # compress
+                        $node =~ s/^y*//;
+                        $node = 'y' if ($node eq '');
+
+                        print "LOG\t$qname\t$qclass\tPTR\t$ttl\t$id\t$nodeprefix$node.$dom\n" if ($debug);
+                        print "DATA\t$qname\t$qclass\tPTR\t$ttl\t$id\t$nodeprefix$node.$dom\n";
+                    }
+                }
+            }
+        } else {
+            print "LOG\tIllegal qtype from PowerDNS\n";
+            print "FAIL\n";
+        }
+        #end of data
+        print "END\n";
+    }
 
