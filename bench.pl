2649000bef161d51111337b75a4cecd9d6496002
Simon Hitzemann <simon-github@hitzemann.org>
2012-05-06 19:33:30 +0200

    Added benchmark for reverting PTR questions. Original was a bit faster and it did not involve regexps.

--- bench.pl
+++ bench.pl
@@ -4,13 +4,13 @@
 use Benchmark qw(:all);
 
 cmpthese (0, {
-        'join' => sub {
+        'join (new)' => sub {
             my $node="bla";
             while (length($node)<56) {
                 $node = join '', "y", $node;
             }
         },
-        'equals' => sub {
+        'equals (old)' => sub {
             my $node="bla";
             while (length($node)<56) {
                 $node = "y$node";
@@ -25,7 +25,7 @@ cmpthese (0, {
     });
 
 cmpthese (0, {
-        'regexp' => sub {
+        'regexp (old)' => sub {
             my $key="f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa";
             my $node="4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa";
             my $qname;
@@ -34,7 +34,7 @@ cmpthese (0, {
                 $node = $1;
             }
         },
-        'substr' => sub {
+        'substr (new)' => sub {
             my $key="f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa";
             my $node="4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa";
             my $qname;
@@ -47,12 +47,12 @@ cmpthese (0, {
     });
 
 cmpthese (0, {
-        'splitreg' => sub {
+        'splitreg (old)' => sub {
             my $string ="Q\t4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa\tIN\tANY\t-1\t127.0.0.1\n";
             chomp $string;
             my @arr=split(/[\t ]+/, $string);
         },
-        'splitplain' => sub {
+        'splitplain (new)' => sub {
             my $string ="Q\t4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f.ip6.arpa\tIN\tANY\t-1\t127.0.0.1\n";
             chomp $string;
             my @arr=split(/\t/, $string);
@@ -60,13 +60,25 @@ cmpthese (0, {
     });
 
 cmpthese (0, {
-        'regexp' => sub {
+        'regexp (old)' => sub {
             my $str = 'deadbeefcafebabe73311234';
             $str =~ s/(.{4})/$1:/g;
             chop $str;
         } ,
-        'join' => sub {
+        'join (new)' => sub {
             my $str = 'deadbeefcafebabe73311234';
             $str = join ':', substr($str, 0, 4), substr($str, 4, 4), substr($str, 8, 4), substr($str, 12, 4), substr($str, 16, 4), substr($str, 20, 4), substr($str, 24, 4), substr($str, 28, 4);
+        }
+    });
+
+cmpthese (0, {
+        'splitjoin (old & new)' => sub {
+            my $node = '4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f';
+            $node = join '', reverse split /\./, $node;
+        },
+        'regex' => sub {
+            my $node = '4.a.9.7.b.9.e.f.0.0.0.0.0.0.0.0.f.f.6.5.0.5.2.0.0.0.0.0.0.8.e.f';
+            $node =~ s/\.//g;
+            $node = scalar reverse $node;
         }
     });
